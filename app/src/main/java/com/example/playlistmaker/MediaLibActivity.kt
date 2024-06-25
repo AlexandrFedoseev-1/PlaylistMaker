71f90a7f1c5709de81847f64405b58a9aa107ad8
AlexandrFedoseev-1 <MrHumanFed@yandex.ru>
2024-06-25 16:53:51 +0300

    Готово

---
+++ app/src/main/java/com/example/playlistmaker/MediaLibActivity.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MediaLibActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_lib)
+    }
+}
