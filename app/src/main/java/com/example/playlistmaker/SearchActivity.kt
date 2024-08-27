7b108fb7871c85afd8511e9f5fecb247d7c1730a
AlexandrFedoseev-1 <MrHumanFed@yandex.ru>
2024-08-27 17:44:55 +0300

    Добавил необходимую логику + немного изменил объявление переменных

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -6,33 +6,83 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.*
-import android.view.inputmethod.InputMethod
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
     private var savedText: String? = null
+    private val trackBaseUrl = BASE_URL
+    private val retrofit = Retrofit.Builder().baseUrl(trackBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+
+    private val trackService = retrofit.create(TrackSearchApi::class.java)
+    private val trackList = ArrayList<Track>()
+
+    private lateinit var placeholderMessage: LinearLayout
+    private lateinit var backButton: ImageView
+    private lateinit var placeholderImage: ImageView
+    private lateinit var searchEditText: EditText
+    private lateinit var clearButton: ImageView
+    private lateinit var searchRecyclerView: RecyclerView
+    private lateinit var updateButton: Button
+    private lateinit var placeholderText: TextView
+
+
+    private val searchAdapter = SearchAdapter(trackList)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        val backButton = findViewById<ImageView>(R.id.back_button)
-        val searchEditText = findViewById<EditText>(R.id.search)
-        val clearButton = findViewById<ImageView>(R.id.clear_text)
-        val searchRecyclerView = findViewById<RecyclerView>(R.id.search_list)
+        backButton = findViewById(R.id.back_button)
+        searchEditText = findViewById(R.id.search)
+        clearButton = findViewById(R.id.clear_text)
+        placeholderImage = findViewById(R.id.placeholder_image)
+        placeholderMessage = findViewById(R.id.placeholder)
+        searchRecyclerView = findViewById(R.id.search_list)
+        updateButton = findViewById(R.id.update_button)
+        placeholderText = findViewById(R.id.placeholder_text)
         searchRecyclerView.layoutManager = LinearLayoutManager(this)
-        val searchAdapter = SearchAdapter(trackList)
         searchRecyclerView.adapter = searchAdapter
+
+        searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                trackSearch()
+                true
+            }
+            false
+        }
+
+
         backButton.setOnClickListener {
             super.finish()
+        }
+
+        updateButton.setOnClickListener{
+            trackSearch()
         }
 
         clearButton.setOnClickListener {
             searchEditText.setText("")
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             imm?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+            trackList.clear()
+            searchAdapter.notifyDataSetChanged()
+            placeholderMessage.visibility =View.GONE
         }
 
         val searchTextWatcher = object : TextWatcher {
@@ -55,47 +105,83 @@ class SearchActivity : AppCompatActivity() {
 
 
     }
+    private fun trackSearch(){
+        if (searchEditText.text.isNotEmpty()) {
+            trackService.search(searchEditText.text.toString())
+                .enqueue(object : Callback<TrackResponse> {
+                    override fun onResponse(
+                        call: Call<TrackResponse>,
+                        response: Response<TrackResponse>
+                    ) {
+                        if (response.code() == 200) {
+                            trackList.clear()
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                trackList.addAll(response.body()?.results!!)
+                                searchAdapter.notifyDataSetChanged()
+                            }
+                            if (trackList.isEmpty()) {
+                                showMessage(
+                                    getString(R.string.placeholder_not_found),
+                                    response.code()
+                                )
+                            } else {
+                                showMessage("", response.code())
+                            }
+                        } else {
+                            showMessage(
+                                getString(R.string.placeholder_error),
+                                response.code()
+                            )
+                        }
+                    }
+
+                    override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                        showMessage(getString(R.string.placeholder_error), null)
+                    }
+                })
+        }
+    }
+
+    private fun showMessage(text: String, code: Int?) {
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            trackList.clear()
+            searchAdapter.notifyDataSetChanged()
+            placeholderText.text = text
+            Toast.makeText(applicationContext, code.toString(), Toast.LENGTH_LONG)
+                .show()
+            if (code == 200) {
+                placeholderImage.setImageDrawable(
+                    AppCompatResources.getDrawable(
+                        this,
+                        R.drawable.not_found
+                    )
+                )
+                updateButton.visibility = View.GONE
+            } else {
+                placeholderImage.setImageDrawable(
+                    AppCompatResources.getDrawable(
+                        this,
+                        R.drawable.search_error
+                    )
+                )
+                updateButton.visibility = View.VISIBLE
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+        }
+    }
+
+    companion object {
+        const val SAVED_TEXT = "SAVED_TEXT"
+        const val BASE_URL = "https://itunes.apple.com"
+    }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(SAVED_TEXT, savedText)
     }
 
-    companion object {
-        const val SAVED_TEXT = "SAVED_TEXT"
-        val trackList = listOf(
-            Track(
-                trackName = "Smells Like Teen Spirit",
-                artistName = "Nirvana",
-                trackTime = "5:01",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Billie Jean",
-                artistName = "Michael Jackson",
-                trackTime = "4:35",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Stayin' Alive",
-                artistName = "Bee Gees",
-                trackTime = "4:10",
-                artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Whole Lotta Love",
-                artistName = "Led Zeppelin",
-                trackTime = "5:33",
-                artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Sweet Child O'Mine",
-                artistName = "Guns N' Roses",
-                trackTime = "5:03",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc48"
-            )
-        )
-    }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
